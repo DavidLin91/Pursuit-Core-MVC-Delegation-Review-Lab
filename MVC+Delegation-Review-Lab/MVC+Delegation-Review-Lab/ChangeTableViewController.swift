1e11d4516b93da103c7857907b9e11945cb8686a
David <lin.david985@gmail.com>
2019-11-20 17:03:47 -0500

    loaded tableviews

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ChangeTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChangeTableViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by David Lin on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ChangeTableViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
