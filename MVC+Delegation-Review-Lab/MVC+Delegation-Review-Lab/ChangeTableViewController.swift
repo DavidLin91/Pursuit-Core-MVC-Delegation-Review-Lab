94f088e99b8387dfadb0a69f9b921dfce80f1764
David <lin.david985@gmail.com>
2019-11-20 17:49:15 -0500

    got stepper slider func to work

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ChangeTableViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ChangeTableViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 
 class ChangeTableViewController: UIViewController {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var slideStepTracker: UILabel!
     
-    
-    
+ 
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+        stepper.maximumValue = 50
     }
     
+    
+     
+     
+    
+    @IBAction func sliderFunc(_ sender: UISlider) {
+        stepper.value = Double(sender.value)
+        slideStepTracker.text = "Preview Font Size: \(Int(sender.value).description)"
+    }
+    
+    @IBAction func stepperFunc(_ sender: UIStepper) {
+        sliderValue.value = Float(sender.value)
+        slideStepTracker.text = " Preview Font Size: \(Int(sender.value).description)"
+        
+        
+    }
 }
